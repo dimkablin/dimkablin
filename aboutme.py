99254d8334ed10965aa63c97f8f0ab663c7a5cff
zanoo <dmitryellison@gmail.com>
2023-03-23 00:07:33 +0300

    added code

---
+++ aboutme.py
@@ -0,0 +1,13 @@
+
+class AboutMe:
+    def __init__(self):
+        self.name = "Dimka"
+        self.education = "ITMO University"
+        self.faculty = "Robotics and Artificial Intelligence"
+        self.languages = ["Python", "C/C++", "Java"]
+
+    def say_hello(self):
+        print("Hi, my name is " + self.name + "!")
+        print("Wanna join my journey?")
+
+
